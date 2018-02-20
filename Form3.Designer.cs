c7b1a84be48443b799aec9e745bf5d5bfa670cb6
Anoosha keen <anookeen@gmail.com>
2018-02-20 16:25:11 +0000

    SC lab2 part 1 and 4

---
+++ Form3.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace library_management_system
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "author",
+            "genre",
+            "name"});
+            this.comboBox1.Location = new System.Drawing.Point(504, 67);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(78, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(319, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(690, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Book Issue";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(690, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Book Return";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(727, 505);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 29);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "logout";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(78, 119);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(547, 415);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 602);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ Form3.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace library_management_system
+{
+    public partial class Form3 : Form
+    {
+        String s;
+        private BindingSource bindingSource1 = new BindingSource();
+        private string username;
+
+        public Form3(string u)
+        {
+            InitializeComponent();
+            username = u;
+            dataGridView1.Columns.Add("column", "Book");
+            dataGridView1.Columns.Add("column", "Author");
+            dataGridView1.Columns.Add("column", "Genre");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            s = comboBox1.Text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "")
+            {
+                textBox1.Text = "";
+                return;
+            }
+
+            SqlConnection sqlConnection1 = new SqlConnection();
+            sqlConnection1.ConnectionString = "Server=AWAN-PC;Database=lms;Trusted_Connection=true;";
+
+            SqlCommand query = new SqlCommand();
+            SqlDataReader reader;
+
+            query.CommandType = CommandType.Text;
+            query.Connection = sqlConnection1;
+
+            String y = textBox1.Text;
+
+            if (s == "genre")
+            {
+                query.CommandText = "SELECT artifactName, author, genre FROM artifact WHERE genre LIKE '" + y + "%'";
+            }
+            if (s == "name")
+            {
+                query.CommandText = "SELECT artifactName, author, genre FROM artifact WHERE artifactName LIKE '" + y + "%'";
+            }
+            if (s == "author")
+            {
+                query.CommandText = "SELECT artifactName, author, genre FROM artifact WHERE author LIKE '" + y + "%'";
+            }
+
+
+
+            try
+            {
+
+                sqlConnection1.Open();
+
+                reader = query.ExecuteReader();
+                this.dataGridView1.DataSource = null;
+                this.dataGridView1.Rows.Clear();
+
+                while (reader.Read()) {
+                    object[] row = { reader[0], reader[1], reader[2] };
+                    dataGridView1.Rows.Add(row);
+                }
+
+                reader.Close();
+                
+                //SqlDataAdapter adapter = new SqlDataAdapter(query.CommandText, sqlConnection1);
+
+                //DataSet ds = new DataSet();
+               // adapter.TableMappings.Add("artifact", "Books");
+                //adapter.Fill(ds);
+                
+                //sqlConnection1.Close();
+                //dataGridView1.DataSource = ds;
+             //   dataGridView1.DataMember = "Books.artifactName";
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 f1 = new Form1();
+            f1.ShowDialog();
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Issue f1 = new Issue(username);
+            f1.ShowDialog();
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Return f1 = new Return(username);
+            f1.ShowDialog();
+            this.Close();
+        }
+    }
+}
+
